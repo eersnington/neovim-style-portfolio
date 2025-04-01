5b6b5aab7482db835c2ee1e15d325ca6270bd8e0
Sree Narayanan <sreeaadhi07@gmail.com>
2025-04-01 18:32:05 +0400

    branch with posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { GeistMono } from "geist/font/mono";
+import { PostHogProvider } from "@/components/PostHogProvider";
 
 import "@/app/globals.css";
 import config from "@/lib/config";
@@ -25,14 +26,16 @@ export default function RootLayout({
         />
       </head>
       <body className={GeistMono.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <PostHogProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </PostHogProvider>
       </body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,24 @@ export default (phase, { defaultConfig }) => {
    */
   const nextConfig = {
     /* config options here */
-  }
-  return nextConfig
-}
+    async rewrites() {
+      return [
+        {
+          source: "/ingest/static/:path*",
+          destination: "https://us-assets.i.posthog.com/static/:path*",
+        },
+        {
+          source: "/ingest/:path*",
+          destination: "https://us.i.posthog.com/:path*",
+        },
+        {
+          source: "/ingest/decide",
+          destination: "https://us.i.posthog.com/decide",
+        },
+      ];
+    },
+    // This is required to support PostHog trailing slash API requests
+    skipTrailingSlashRedirect: true,
+  };
+  return nextConfig;
+};
