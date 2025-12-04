b708cbcd00349746bcd32fba135c3e5c658e2aca
Sree Narayanan <sreeaadhi07@gmail.com>
2025-12-04 19:20:23 +0400

    Update subtitle in config.ts for clarity

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ import type { Config } from './types';
 const config: Config = {
   pageTitle: 'Sree N | @eersnington',
   title: 'Sree',
-  subtitle: 'SWE @ StockPenguins | I like breaking web and building games',
+  subtitle: 'SWE | I like to build games and mess with web tech',
 
   links: [
     {
